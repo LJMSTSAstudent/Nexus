356ac6276a802417f456c9e9a894e9178a858e1b
LJMSTSAstudent <162632476+LJMSTSAstudent@users.noreply.github.com>
2026-01-19 08:41:17 -0500

    Simplify swiper configuration in script.js
    Removed unnecessary swiper configuration options.

--- script.js
+++ script.js
@@ -2,16 +2,6 @@ const swiperBusinesses = new Swiper('.businesses__swiper', {
    loop: true,
    slidesPerView: 'auto',
    centeredSlides: 'auto',
-   spaceBetween: 16,
-   grabCursor: true,
-   speed: 600,
-   effect: 'coverflow',
-   coverflowEffect:{
-      rotate: -90,
-      depth: 600,
-      modifier: .5,
-      slideShadows: false,
-   },
   
   pagination: {
     el: '.swiper-pagination',
